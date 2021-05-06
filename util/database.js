6d7c1cbb95adab3236a7cf0c88a35fb18106cd5b
Heather Stratton <hastratton@gmail.com>
2021-05-06 18:24:36 -0400

    tutorial through updating product

--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+
+  MongoClient.connect('mongodb+srv://heatherS:rzdW8iGaPSvM35rv@cluster0.3uz0q.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected');
+      _db = client.db();
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+exports.mongoConnect= mongoConnect;
+exports.getDb = getDb;

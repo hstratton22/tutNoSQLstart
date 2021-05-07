4e158519c3e8c49b6337cab6f2a7b327fe80036c
Heather Stratton <hastratton@gmail.com>
2021-05-07 10:31:13 -0400

    details

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const mongoConnect = require('./util/database').mongoConnect;
+//const User = require('./models/user');
 
 const app = express();
 
@@ -14,11 +15,12 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { listenerCount } = require('events');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   User.findById("60947956b893eb8bf3e04661")
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
@@ -26,6 +28,7 @@ app.use((req, res, next) => {
     })
     .catch(err => console.log(err));
 });
+*/
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -35,7 +38,14 @@ app.use(errorController.get404);
   console.log(client);
   app.listen(3000);
 });*/
-mongoConnect(() => {
+/*mongoConnect(() => {
   
   app.listen(3000);
-});
+});*/
+mongoose
+  .connect('mongodb+srv://heatherS:rzdW8iGaPSvM35rv@cluster0.3uz0q.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(result => {
+    app.listen(3000);
+  }).catch(err => {
+    console.log(err);
+  })
--- models/product.js
+++ models/product.js
@@ -1,4 +1,25 @@
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const productSchema = new Schema({
+  title:{
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required:true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String, 
+    required:true
+  }
+});
+module.exports = mongoose.model('Product', productSchema);
+/*const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -76,3 +97,4 @@ class Product {
 }
 
 module.exports = Product;
+*/
